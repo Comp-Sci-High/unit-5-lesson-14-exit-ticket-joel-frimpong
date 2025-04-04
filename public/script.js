b3ae0fcd763cd2bcebb2d57016933c5046eb7c89
joel-frimpong <joel.frimpong25@compscihigh.org>
2025-04-04 19:12:13 +0000

    Hi

--- public/script.js
+++ public/script.js
@@ -1,7 +1,10 @@
 // Create a function that tests the post route handler [2 pts]
-
-
+async function test(post){
+    const response = await fetch(base+"/create-potion")
+    const data = await response.json()
+    console.log(data)
+}
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
-
+test()
